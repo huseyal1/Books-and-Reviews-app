f44791e8c3dc53cb6f6f735b57fc31e04a48f038
Ali Huseynzade <huseyal1@fit.cvut.cz>
2025-01-12 20:01:14 +0100

    build.gradle.kts changed

--- TheProject/build.gradle.kts
+++ TheProject/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "ctu.fit.tjv.semester_work"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 repositories {
     mavenCentral()
 }

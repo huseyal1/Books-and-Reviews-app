826d86c86a3a19fbaff8a8ef30f3ca120bf1505c
Ali Huseynzade <huseyal1@fit.cvut.cz>
2025-01-12 21:04:39 +0100

    Book class added to the source code

---
+++ TheProject/src/main/java/ctu/fit/tjv/semester_work/model/Book.java
@@ -0,0 +1,94 @@
+package ctu.fit.tjv.semester_work.model ;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long book_id ;
+    private String title ;
+    private String author ;
+    private Integer publication_year ;
+    private String description ;
+    private String category_name ;
+
+    @Column(unique = true)
+    private Long ISBN ;
+
+    /* Constructors : */
+
+    public Book() {
+        // Default constructor.
+    }
+
+    public Book(String title, String author, Integer publication_year,
+                String description, String category_name, Long ISBN) {
+
+        this.title = title ;
+        this.author = author ;
+        this.publication_year = publication_year ;
+        this.description = description ;
+        this.category_name = category_name ;
+        this.ISBN = ISBN ;
+    }
+
+    public Long getBook_id() {
+        return book_id ;
+    }
+
+    public void setBook_id(Long newBook_id) {
+        this.book_id = newBook_id ;
+    }
+
+    public String getTitle() {
+        return title ;
+    }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle ;
+    }
+
+    public String getAuthor() {
+        return author ;
+    }
+
+    public void setAuthor(String newAuthor) {
+        this.author = newAuthor ;
+    }
+
+    public Integer getPublication_year() {
+        return publication_year ;
+    }
+
+    public void setPublication_year(Integer newPublication_year) {
+        this.publication_year = newPublication_year ;
+    }
+
+
+    public String getDescription() {
+        return description ;
+    }
+
+    public void setDescription(String newDescription) {
+        this.description = newDescription ;
+    }
+
+    public String getCategory_name() {
+        return category_name ;
+    }
+
+    public void setCategory_name(String newCategory_name) {
+        this.category_name = newCategory_name ;
+    }
+
+    public Long getISBN() {
+        return ISBN ;
+    }
+
+    public void setISBN(Long newISBN) {
+        this.ISBN = newISBN ;
+    }
+
+}
